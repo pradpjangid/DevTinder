028d5ce708f949ba5e539b775a13c04db280753d
pradeep jangid <pradeepjangid661@gmail.com>
2026-04-19 15:24:45 +0530

    kh

--- src/app.js
+++ src/app.js
@@ -6,17 +6,37 @@ import { User } from "./models/user.js";
 dotenv.config();
 const app = express();
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "sdfsdfs",
-    lastName: "sdfsdf",
-    emailId: "sdfsdf@gmail.com",
-    password: "sdfsdf#2314",
-    gender: "sddddddddd",
-    age: 32,
-  });
+  const user = new User(req.body);
   try {
     await user.save();
+    res.send("User Added Succesffu");
+  } catch (error) {
+    res.status(400).send("Error saving the user:", error);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const emaildId = req.body.emailId;
+
+  const resthis = await User.findOne({ emailId: emaildId });
+  res.send(resthis);
+});
+app.get("/feed", async (req, res) => {
+  try {
+    const allFeed = await User.find({});
+    res.send(allFeed);
+  } catch (error) {
+    res.status(400).send("Something Went Wrong:", error);
+  }
+});
+
+app.post("/update", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    await User.findByIdAndUpdate(userId, req.body);
     res.send("User Added Succesffu");
   } catch (error) {
     res.status(400).send("Error saving the user:", error);
